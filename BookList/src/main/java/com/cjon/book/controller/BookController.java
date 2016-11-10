041400f0874fb737735f59573aa8aa78a82ace53
choi sae mi na <alsk713@naver.com>
2016-11-10 18:06:32 +0900

    SpringDailyWork3

---
+++ BookList/src/main/java/com/cjon/book/controller/BookController.java
@@ -0,0 +1,50 @@
+package com.cjon.book.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.cjon.book.dto.BookDTO;
+import com.cjon.book.service.BookSearchKeywordList;
+import com.cjon.book.service.BookService;
+
+@Controller
+public class BookController {
+
+	@RequestMapping("/bookList")
+	//JSONP방식으로 JSON데이터를 보내줌
+	
+	public void getBookList(HttpServletRequest request, HttpServletResponse response){
+		
+		String keyword = request.getParameter("keyword");
+		String callback = request.getParameter("callback");
+		
+		BookDTO dto = new BookDTO();
+		dto.setBtitle(keyword);
+		
+		//서비스 객체 생성
+		BookService service = new BookSearchKeywordList();
+		String result = service.execute(dto);
+		
+		response.setContentType("text/plain; charset=utf8");
+		PrintWriter out;
+		try {
+			out = response.getWriter();
+			
+			out.println(callback + "(" + result + ")");
+			out.flush();
+			out.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} 
+		
+		
+	}
+}
---
+++ BookList/src/main/java/com/cjon/book/service/BookService.java
@@ -0,0 +1,8 @@
+package com.cjon.book.service;
+
+import com.cjon.book.dto.BookDTO;
+
+public interface BookService {
+
+	public String execute(BookDTO entity);
+}
